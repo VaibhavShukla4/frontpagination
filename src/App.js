17003f7c1ed97f3b44e479cef8a2387a1740815f
VaibhavShukla4 <96537580+VaibhavShukla4@users.noreply.github.com>
2023-08-31 12:10:51 +0530

    done

--- src/App.js
+++ src/App.js
@@ -4,64 +4,6 @@
 //   MdOutlineKeyboardArrowRight,
 //   MdOutlineKeyboardArrowLeft,
 // } from "react-icons/md";
-// const App = () => {
-//   const [items, setItems] = useState([]);
-//   const [currentPage, setCurrentPage] = useState(1);
-//   const [totalPages, setTotalPages] = useState(1);
-
-//   useEffect(() => {
-//     fetchItems(currentPage);
-//   }, [currentPage]);
-
-//   const fetchItems = async (page) => {
-//     try {
-//       const response = await axios.get(
-//         `http://localhost:3000/api/items?page=${page}`
-//       );
-//       setItems(response.data.items);
-//       setTotalPages(response.data.totalPages);
-//     } catch (error) {
-//       console.error("Error fetching items:", error);
-//     }
-//   };
-
-//   const handleNextPage = () => {
-//     if (currentPage < totalPages) {
-//       setCurrentPage(currentPage + 1);
-//     }
-//   };
-
-//   const handlePrevPage = () => {
-//     if (currentPage > 1) {
-//       setCurrentPage(currentPage - 1);
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <ul>
-//         {items.map((item) => (
-//           <li key={item.id}>{item.name}</li>
-//         ))}
-//       </ul>
-//       <button onClick={handlePrevPage}>
-//         <MdOutlineKeyboardArrowLeft />
-//       </button>
-//       <button onClick={handleNextPage}>
-//         <MdOutlineKeyboardArrowRight />
-//       </button>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-// import React, { useState, useEffect } from "react";
-// import axios from "axios";
-// import {
-//   MdOutlineKeyboardArrowRight,
-//   MdOutlineKeyboardArrowLeft,
-// } from "react-icons/md";
 // import "./App.css";
 // const App = () => {
 //   const [items, setItems] = useState([]);
